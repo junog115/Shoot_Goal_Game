e009c2d47860c7f9ec1b8cc41d0ecb41770cc6f9
Moomyung <moomyung@Moomyungui-MacBook-Pro.local>
2017-02-08 00:12:47 +0900

    add Process of Press Keyboard

--- Shoot_Goal_Game.c
+++ Shoot_Goal_Game.c
@@ -14,7 +14,7 @@ typedef struct _PLAYER
 PLAYER g_sPlayer;
 char g_sPlayerForm[] = "┖━●━┚";
 int g_nPlayerLength;
-char g_sPrintPlayerPosition[40] = {0};
+char g_sPrintPlayerPosition[40] = { 0 };
 
 
 void Init();
@@ -49,11 +49,33 @@ void Init()
 	g_sPlayer.nPrintX = (g_sPlayer.nMoveX - g_sPlayer.nCenterX);
 	g_sPlayer.nPrintY = (g_sPlayer.nMoveY);
 	g_nPlayerLength = strlen(g_sPlayerForm);
-
 }
 
 void Update()
 {
+	if (kbhit())
+	{
+		switch (getch())
+		{
+		case 'j':
+			g_sPlayer.nMoveX--;
+			break;
+
+		case 'l':
+			g_sPlayer.nMoveX++;
+			break;
+
+		case 'q':
+			exit(0);
+
+		default:
+			break;
+		}
+	}
+	
+	g_sPlayer.nPrintX = (g_sPlayer.nMoveX - g_sPlayer.nCenterX);
+	g_sPlayer.nPrintY = (g_sPlayer.nMoveY);
+	
 	sprintf(g_sPrintPlayerPosition, "주인공의 위치 : %d, %d", g_sPlayer.nMoveX, g_sPlayer.nMoveY);
 
 }
@@ -72,4 +94,3 @@ void Release()
 {
 
 }
-
